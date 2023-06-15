a9f7cc41746f08af9d3398d3dd467e9595a24694
Cayetano Ponce de Leon Molina <cayetano@MacBook-Air-de-Cayetano.local>
2023-06-15 17:38:37 +0200

    css

---
+++ src/hooks/useHeader.jsx
@@ -0,0 +1,9 @@
+function useHeader() {
+  const userLog = () => {};
+  const wishList = () => {};
+  const messages = () => {};
+  const search = () => {};
+  const addNewProduct = () => {};
+  return { userLog, wishList, messages, search, addNewProduct };
+}
+export default useHeader;
