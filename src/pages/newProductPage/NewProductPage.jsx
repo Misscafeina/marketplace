a55de3256ae692995bcc2712df6354b7c42da110
Isabel PM <ispemaz@gmail.com>
2023-06-16 22:32:58 +0200

    new product

--- src/pages/newProductPage/NewProductPage.jsx
+++ src/pages/newProductPage/NewProductPage.jsx
@@ -1,1 +1,27 @@
 import "./style.css";
+import { useContext } from "react";
+import LoginForm from "../../components/users/loginForm/LoginForm";
+import "./style.css";
+import { PopUpContext } from "../../context/popUpContext";
+import RegisterForm from "../../components/users/registerForm/RegisterForm";
+import NewProductForm from "../../components/products/newProductForm/NewProductForm";
+
+function NewProductPage() {
+  const {
+    showPopUp,
+    setShowPopUp,
+    loginActive,
+    registerActive,
+    newProductActive,
+  } = useContext(PopUpContext);
+
+  if (loginActive) {
+    return <div>{showPopUp ? <LoginForm /> : null}</div>;
+  } else if (registerActive) {
+    return <div>{showPopUp ? <RegisterForm /> : null}</div>;
+  } else if (newProductActive) {
+    return <div> {showPopUp ? <NewProductForm /> : null}</div>;
+  }
+}
+
+export default NewProductPage;
