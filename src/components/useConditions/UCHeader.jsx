5e8415c0de3c2b5cfc355ad1957174ec64c06315
Isabel PM <ispemaz@gmail.com>
2023-06-20 22:30:03 +0200

    footer

---
+++ src/components/useConditions/UCHeader.jsx
@@ -0,0 +1,13 @@
+import ThemeSwitcher from "../inputs/ThemeSwitcher";
+import Title from "./Title";
+
+const UCHeader = () => {
+  return (
+    <header>
+      <Title />
+      <ThemeSwitcher />
+    </header>
+  );
+};
+
+export default UCHeader;
